2f0cbd07b7443bf2eb64c6ec0434460ac44605d9
amir <118857034+ABHVBJGCNVSTU@users.noreply.github.com>
2024-05-05 18:47:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
-global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
+global.github=process.env.GITHUB|| "https://github.com/ABHVBJGCNVSTU/BIZODE";
 global.gurl  =process.env.GURL  || "https://chat.whatsapp.com/JxJY873QyUU65Up2DvDSO1";
 global.website=process.env.GURL || "https://chat.whatsapp.com/JxJY873QyUU65Up2DvDSO1" ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/da1876f8eebf102fcf10f.jpg" ; // SET LOGO FOR IMAGE 
